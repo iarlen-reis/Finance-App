1d7b53e097ac67ca5d7b5c07179c01a9e7e59783
Iarlen <iarlensr@hotmail.com>
2024-05-01 17:23:00 -0300

    feat: create tokens for colors and font-family.

---
+++ src/themes/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+  red: "#FF1B1C",
+  white: "#FFFFFF",
+  spring: "#21FA90",
+  errie_black: "#262626",
+  caroline_blue: "#74B3CE",
+  sub_text: "rgba(38, 38, 38, 0.3)",
+};
---
+++ src/themes/fontFamily.ts
@@ -0,0 +1,5 @@
+export const fontFamily = {
+  Sora_400Regular: "Sora_400Regular",
+  Sora_500Medium: "Sora_500Medium",
+  Sora_700Bold: "Sora_700Bold",
+};
